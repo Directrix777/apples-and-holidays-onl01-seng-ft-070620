50918177b1c1f14a0e7c98c0862da26924aa6dfe
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-10 18:19:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, secondary_hash|
-    puts "#{season.capitalize}: "
+    puts "#{season.capitalize}:"
     secondary_hash.each do |holiday, supplies|
       print "#{holiday.split("_").join(" ")}: "
       supplies.each do |supply|
